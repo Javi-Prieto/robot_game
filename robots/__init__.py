4d690a06e62222e4e025ffa2257d9c5265a50a5f
Javier Prieto <jpojaviprior@gmail.com>
2024-01-24 14:09:51 +0100

    added the side walls and the game lose mechanic

--- robots/__init__.py
+++ robots/__init__.py
@@ -1,5 +1,5 @@
 import pygame
-
+import time
 from robots.models.player import Player
 from robots.models.wall import Wall
 
@@ -18,35 +18,80 @@ heart_png = pygame.image.load("assets/heart.png")
 clock = pygame.image.load("assets/clock.png")
 
 player = Player(sprite)
-big_wall_top = Wall(bw_top, [0, 0], [800, 50])
-big_wall_bottom = Wall(bw_bt, [0, 550], [800, 50])
-big_wall_left = Wall(bw_lft, [0, 0], [50, 800])
-big_wall_right = Wall(bw_rght, [750, 0], [50, 800])
+big_wall_top = Wall(bw_top, [0, 0], [800, 50], 0)
+big_wall_bottom = Wall(bw_bt, [0, 550], [800, 50], 1)
+big_wall_left = Wall(bw_lft, [0, 0], [50, 800], 2)
+big_wall_right = Wall(bw_rght, [750, 0], [50, 800], 3)
 font = pygame.font.Font(None, 50)
 
 running = True
 surface = pygame.Surface((surface_width, surface_height))
 screen = pygame.display.set_mode((surface_width, screen_height))
+is_moving_up = False
+is_moving_down = False
+is_moving_left = False
+is_moving_right = False
+
+
+def check_player_life():
+    if not player.life > 0:
+        global running
+        running = False
+
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             match event.key:
                 case pygame.K_RIGHT:
-                    player.move_right()
+                    is_moving_right = True
                 case pygame.K_LEFT:
-                    player.move_left()
+                    is_moving_left = True
                 case pygame.K_UP:
-                    player.move_up()
+                    is_moving_up = True
                 case pygame.K_DOWN:
-                    player.move_down()
-        if player.wall_collision(big_wall_top.position, big_wall_top.size):
+                    is_moving_down = True
+        elif event.type == pygame.KEYUP:
+            match event.key:
+                case pygame.K_RIGHT:
+                    is_moving_right = False
+                case pygame.K_LEFT:
+                    is_moving_left = False
+                case pygame.K_UP:
+                    is_moving_up = False
+                case pygame.K_DOWN:
+                    is_moving_down = False
+
+    if is_moving_up:
+        if not player.wall_collision(big_wall_top):
+            player.move_up()
+        else:
+            time.sleep(0.1)
             player.life -= 1
-        elif  player.wall_collision(big_wall_top.position, big_wall_top.size):
+    elif is_moving_down:
+        if not player.wall_collision(big_wall_bottom):
+            player.move_down()
+        else:
+            time.sleep(0.1)
+            player.life -= 1
+    elif is_moving_left:
+        if not player.wall_collision(big_wall_left):
+            player.move_left()
+        else:
+            time.sleep(0.1)
+            player.life -= 1
+    elif is_moving_right:
+        if not player.wall_collision(big_wall_right):
+            player.move_right()
+        else:
+            time.sleep(0.1)
             player.life -= 1
 
+    check_player_life()
 
-    life_text = font.render('x '+str(player.life), True, (255, 255, 255))
+    life_text = font.render('------------', True, (0, 0, 0), (0, 0, 0))
+    screen.blit(life_text, (40, 7))
+    life_text = font.render('x ' + str(player.life), False, (255, 255, 255))
     screen.blit(life_text, (40, 7))
     screen.blit(heart_png, (5, 10))
     screen.blit(surface, (0, 50))
--- robots/models/player.py
+++ robots/models/player.py
@@ -1,8 +1,11 @@
+from robots.models.wall import Wall
+
+
 class Player():
     def __init__(self, sprite):
-        self.position = [200, 10]
-        self.speed = 10
-        self.size = [100, 100]
+        self.position = [100, 60]
+        self.speed = 1
+        self.size = [70, 70]
         self.sprite = sprite
         self.life = 10
 
@@ -18,7 +21,20 @@ class Player():
     def move_down(self):
         self.position[1] += self.speed
 
-    def wall_collision(self, wall_position, wall_size):
-        if(self.position[0] == wall_position[0] & self.position[1] == wall_position[0]):
-            return True
+    def wall_collision(self, wall: Wall):
+        match wall.looking_at:
+            case 0:
+                if self.position[1] <= wall.position[1] + wall.size[1]:
+                    return True
+            case 1:
+                if self.position[1] >= wall.position[1] - wall.size[1]:
+                    return True
+            case 2:
+                if self.position[0] <= wall.position[0] + wall.size[0]:
+                    return True
+            case 3:
+                if self.position[0] >= wall.position[0] - wall.size[0]:
+                    return True
+            case _:
+                return False
         return False
