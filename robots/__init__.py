6b7745ba958b8b91463924f427d623597dab9033
Javier Prieto <jpojaviprior@gmail.com>
2024-01-24 22:21:01 +0100

    Added the water block which make damage every move on it and the waterproof potion

--- robots/__init__.py
+++ robots/__init__.py
@@ -1,31 +1,32 @@
 import pygame
 import time
 from robots.models.player import Player
+from robots.models.potion import WaterPotion
 from robots.models.wall import Wall
+from robots.models.water import Water
 
 pygame.init()
 screen_height = 650
 surface_width = 800
 surface_height = 600
 bg = pygame.image.load("assets/background.jpg")
-sprite = pygame.image.load("assets/PlayerSprites/PlayerFrontStay.png")
-sprite_front = pygame.image.load("assets/PlayerSprites/PlayerFrontWalking.png")
-sprite_back = pygame.image.load("assets/PlayerSprites/PlayerBackWalking.png")
-sprite_right = pygame.image.load("assets/PlayerSprites/PlayerRightWalking.png")
-sprite_left = pygame.image.load("assets/PlayerSprites/PlayerLeftWalking.png")
+
 wall = pygame.image.load("assets/wall.png")
 bw_top = pygame.image.load("assets/big_wall_top.png")
 bw_bt = pygame.image.load("assets/big_wall_bottom.png")
 bw_lft = pygame.image.load("assets/big_wall_left.png")
 bw_rght = pygame.image.load("assets/big_wall_right.png")
 heart_png = pygame.image.load("assets/heart.png")
-clock = pygame.image.load("assets/clock.png")
+clock_png = pygame.image.load("assets/clock.png")
 
-player = Player(sprite)
-big_wall_top = Wall(bw_top, [0, 0], [800, 50], 0)
-big_wall_bottom = Wall(bw_bt, [0, 550], [800, 50], 1)
-big_wall_left = Wall(bw_lft, [0, 0], [50, 800], 2)
-big_wall_right = Wall(bw_rght, [750, 0], [50, 800], 3)
+clock = pygame.time.Clock()
+player = Player()
+big_wall_top = Wall(bw_top, [0, 0], [800, 20], 0)
+big_wall_bottom = Wall(bw_bt, [0, 550], [800, 20], 1)
+big_wall_left = Wall(bw_lft, [0, 0], [30, 800], 2)
+big_wall_right = Wall(bw_rght, [750, 0], [0, 800], 3)
+water_block1 = Water([500, 400], [62, 62])
+water_potion = WaterPotion([300, 200])
 font = pygame.font.Font(None, 50)
 
 running = True
@@ -44,7 +45,10 @@ def check_player_life():
 
 
 while running:
+    clock.tick(120)
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.KEYDOWN:
             match event.key:
                 case pygame.K_RIGHT:
@@ -67,36 +71,45 @@ while running:
                     is_moving_down = False
 
     if is_moving_up:
-        player.sprite = sprite_back
         if not player.wall_collision(big_wall_top):
             player.move_up()
+            if player.water_collision(water_block1):
+                time.sleep(0.1)
+                player.life -= 1
         else:
             time.sleep(0.1)
             player.life -= 1
     elif is_moving_down:
-        player.sprite = sprite_front
         if not player.wall_collision(big_wall_bottom):
             player.move_down()
+            if player.water_collision(water_block1):
+                time.sleep(0.1)
+                player.life -= 1
         else:
             time.sleep(0.1)
             player.life -= 1
     elif is_moving_left:
-        player.sprite = sprite_left
         if not player.wall_collision(big_wall_left):
             player.move_left()
+            if player.water_collision(water_block1):
+                time.sleep(0.1)
+                player.life -= 1
         else:
             time.sleep(0.1)
             player.life -= 1
     elif is_moving_right:
-        player.sprite = sprite_right
         if not player.wall_collision(big_wall_right):
             player.move_right()
+            if player.water_collision(water_block1):
+                time.sleep(0.1)
+                player.life -= 1
         else:
             time.sleep(0.1)
             player.life -= 1
-
+    player.collectable_collision(water_potion)
     check_player_life()
 
+    print(player.isWaterproof)
     life_text = font.render('------------', True, (0, 0, 0), (0, 0, 0))
     screen.blit(life_text, (40, 7))
     life_text = font.render('x ' + str(player.life), False, (255, 255, 255))
@@ -104,6 +117,8 @@ while running:
     screen.blit(heart_png, (5, 10))
     screen.blit(surface, (0, 50))
     surface.blit(bg, (0, 0))
+    surface.blit(water_potion.sprite, tuple(water_potion.position))
+    surface.blit(water_block1.sprite, tuple(water_block1.position))
     surface.blit(big_wall_top.sprite, tuple(big_wall_top.position))
     surface.blit(big_wall_bottom.sprite, tuple(big_wall_bottom.position))
     surface.blit(big_wall_left.sprite, tuple(big_wall_left.position))
@@ -111,8 +126,5 @@ while running:
     surface.blit(player.sprite, (player.position[0], player.position[1]))
 
     pygame.display.flip()
-
-    if event.type == pygame.QUIT:
-        running = False
 
 pygame.quit()
